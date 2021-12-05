9366b0fe153eef61301c4b083be2250f876e54d1
diaca89 <85277505+diaca89@users.noreply.github.com>
2021-12-05 16:52:23 +0100

    commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+function Cookies(functions) {
+  const container = document.querySelector('.cookies-container');
+  const save = document.querySelector('.cookies-save');
+
+  function getFormPref() {
+    const inputs = Array.from [document.querySelectorAll('[data-function]')];
+    console.log(inputs);
+  }
+
+function handleSave() {
+  const pref = getFormPref ();
+}
+
+
+
+save.addEventListener('click', handleSave);
+}
+
+
+var check = document.querySelector(".check");
+check.addEventListener('click', idioma);
+
+function idioma() {
+  let id = check.checked;
+  if (id == true) {
+    location.href = "es/index.html";
+  } else {
+    location.href = "../index.html";
+  }
+}
