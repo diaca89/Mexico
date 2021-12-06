5e87db9b4fbb3b7d0f0268fff22a3b7ddf46535a
diaca89 <85277505+diaca89@users.noreply.github.com>
2021-12-06 17:12:38 +0100

    json

--- js/app.js
+++ js/app.js
@@ -1,16 +1,54 @@
 function Cookies(functions) {
   const container = document.querySelector('.cookies-container');
   const save = document.querySelector('.cookies-save');
+if (!container || !save) return null;
+
+
+const localFormPref = JSON.parse(window.localStorage.getItem('cookies-pref'));
+if (localPref) activateFunctions(localPref);
+
+
 
   function getFormPref() {
-    const inputs = Array.from [...document.querySelectorAll('[data-function]')];
-    console.log (inputs);
+    return [document.querySelectorAll('[data-function]')]
+  .filter ((el) => el.checked)
+  .map( (el) => el.getAttribute('data-functions'));
+
   }
+
+
+function activateFunctions(pref){
+  pref.forEach((f) => functions[f]());
+  container.style.display = 'none';
+window.localStorage.setItem('cookies-pref', JSON.stringify(pref))
+;
+}
+
+
 
 function handleSave() {
   const pref = getFormPref ();
+  activateFunctions(pref);
 }
 
+
+save.addEventListener('click', handleSave);
+
+function marketing(){
+  console.log('function de marketing')
+
+}
+
+function analytics(){
+  console.log('function de analitica')
+
+}
+
+ cookies({
+   marketing: 's',
+   analytics: 'a',
+
+ });
 
 
 save.addEventListener('click', handleSave);
